ff4574a0ec6d5eee5c92838e4e8dff19fd79071f
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-12-15 23:51:56 +0900

    chore: add example code

--- example/main.py
+++ example/main.py
@@ -1,8 +1,10 @@
-from falcon_vue import register_vue_app
 import os
+import sys
 import falcon
 from wsgiref import simple_server
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from falcon_vue import register_vue_app
 
 app = falcon.API()
 
@@ -10,7 +12,7 @@ app = register_vue_app(
     app,
     api_url="/test/hoge",
     # api_url="/",
-    src_path="./test_project/dist"
+    src_path="../test_project/dist"
 )
 
 if __name__ == '__main__':
